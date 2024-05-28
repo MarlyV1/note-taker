f188c0dd54552e5ffd59ce26893c0995aeb7ff23
Marly Valcin <marlyvalcin@gmail.com>
2024-05-27 22:02:41 -0400

    added static middleware to server.js and added GET and POST requests to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,11 @@
 const notes = require('express').Router();
-const notesHTML = require('../public/notes.html')
+const notesDB = require('../db/db.json')
 
-notes.get('/', (req, res) => res.json(notesHTML) )
+
+notes.get('/', (req, res) => res.json(notesDB))
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+})
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const apiRouter = require('./routes');
 
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
