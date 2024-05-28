1d265cb3404302a363baabcfbfda55d013f6a787
Marly Valcin <marlyvalcin@gmail.com>
2024-05-27 20:40:39 -0400

    added a variable for the route in the index file and importes the routes into server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const apiRouter = require('./routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
-
-
+app.use('/api', apiRouter);
 
 
 app.listen(PORT, () => {
