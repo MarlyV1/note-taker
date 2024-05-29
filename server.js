126d5c26276d22d3d250d1708300b7e1a41ec4ea
Marly Valcin <marlyvalcin@gmail.com>
2024-05-29 00:15:21 -0400

    added route for the notes page and a fallback route

--- server.js
+++ server.js
@@ -2,14 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const apiRouter = require('./routes');
+const path = require('path');
 
-app.use(express.static('public'));
+//Middleware for parsing JSON and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
-
 app.use('/api', apiRouter);
 
+app.use(express.static('public'));
+
+//GET route for the notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+//Fallback route made for when a user attempts to visit a nonexistent route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
-})
+});
