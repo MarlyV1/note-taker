893a398b899bc84f546f912b4c3c649768e438d8
Marly Valcin <marlyvalcin@gmail.com>
2024-05-29 00:01:13 -0400

    updated the readAndAppendFile function and added comments

--- routes/notes.js
+++ routes/notes.js
@@ -3,8 +3,10 @@ const notesDB = require('../db/db.json')
 const fs = require('fs');
 const { v4: uuid } = require('uuid');
 
+//GET route to retrieve all notes
 notes.get('/', (req, res) => res.json(notesDB))
 
+//POST route to add a new note
 notes.post('/', (req, res) => {
     const { title, text } = req.body;
 
@@ -19,6 +21,7 @@ notes.post('/', (req, res) => {
     return res.json(newNote);
 });
 
+//Retrieves all notes then writes the new note to the file
 function readAndAppendFile(newNote) {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         err ? console.error(err) : console.log(data);
@@ -26,13 +29,11 @@ function readAndAppendFile(newNote) {
         parsedData.push(newNote)
         console.log(parsedData);
 
-        writeFile('./db/db.json', parsedData)
+    fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) => {
+        err ? console.error(err) : console.log("Successfully wrote to the file")})
     });
-}
+};
 
-function writeFile(file, data) {
-    fs.writeFile(file, JSON.stringify(data, null, 4), (err) => err ? console.error(err) : console.log("Successfully wrote to the file"))
-}
 
 
 module.exports = notes;
