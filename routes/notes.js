9a64fe08f6ecff75d7476df26202880209f407c6
Marly Valcin <marlyvalcin@gmail.com>
2024-05-28 21:40:21 -0400

    updated the POST request for the notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 const notes = require('express').Router();
 const notesDB = require('../db/db.json')
-
+const fs = require('fs');
+const { v4: uuid } = require('uuid');
 
 notes.get('/', (req, res) => res.json(notesDB))
 
@@ -9,8 +10,18 @@ notes.post('/', (req, res) => {
 
     const newNote = {
         title,
-        text
+        text,
+        note_id: uuid()
     };
+
+    // console.log(newNote);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        err ? console.error(err) : console.log(data);
+        const parsedData = JSON.parse(data);
+        parsedData.push(newNote)
+        console.log(parsedData);
+    })
+    return res.json(newNote);
 })
 
 module.exports = notes;
